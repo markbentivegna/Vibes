17298163749e83280d83459eb85b8aee8cae794b
Mark Bentivegna <markbentivegna@gmail.com>
2018-05-15 08:18:30 -0400

    Created project and login UI flow

--- Vibes/Controllers/SignUpViewController.swift
+++ Vibes/Controllers/SignUpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignUpViewController.swift
 //  Vibes
 //
 //  Created by Mark Bentivegna on 5/14/18.
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func createAccountPressed(_ sender: UIButton) {
+    }
+    
 }
 
